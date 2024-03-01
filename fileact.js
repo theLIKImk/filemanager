2c90a22e3eb7101430283c2facab44df7f06b6b6
LIKImk <103368018+theLIKImk@users.noreply.github.com>
2024-03-01 16:59:12 +0800

    Add files via upload

---
+++ fileact.js
@@ -0,0 +1,160 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const util = require('util');
+//local modules
+const ejs = require('ejs');
+const formidable = require('formidable');
+
+//设定
+const ___dir = "./www";
+const port = 80;
+___path = "";
+___url="";
+userg=[
+	{user: 'admin', passwd: 'admin'},
+	{user: 'user', passwd: '123465'}
+];
+userLoginUUID=[];
+
+http.createServer(function (req, res) {
+	url_name = url.parse(req.url,true).pathname;
+	url_val = url.parse(req.url,true).query;
+	url_allval = url.parse(req.url,true).search;
+		
+	if (url_name == "/"){
+		indexHTML(res,req);
+		return;
+	} else if ( url_name == "/login" ){
+		login(res,req);
+		return;
+	} else if ( url_name == "/upload" ){
+		upload(res,req);
+		return;
+	}
+
+ 
+	___url=url_name;
+	___path = ___dir+___url;
+	console.log(___path);
+	loadfile(res,req);
+}).listen(port);
+
+async function login(res,req){
+	//异步转同步
+	const form = new formidable.IncomingForm();
+	const formParseAsync = util.promisify(form.parse.bind(form));
+	
+	// 解析表单数据
+	var formData = null;
+	formData = await formParseAsync(req);
+	
+	console.log("[LOGIN FORM] " + formData.user + "-" + formData.passwd);
+	
+	res.writeHead(200, {'Content-Type': 'text/json'});
+	
+	//解析用户列表
+	for (var i=0 ; i < userg.length; i++){
+		if(formData.user==userg[i].user && formData.passwd==userg[i].passwd) {
+			
+			//创建会话ID
+			var getloginUUID=getUuid();
+			console.log("Login OK! ID: " + getloginUUID);
+			userLoginUUID[getloginUUID]=true;
+			res.end('{"login":"0","msg":"","loginUUID":"' + getloginUUID + '"}');
+			return;
+		}
+	}
+	console.log('Login Fail！');
+	res.end('{"login":"1","msg":"Login Fail！"}');
+    return;
+}
+
+function getUuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+function indexHTML(res,req){
+	//使用 ejs 动态页面模板引擎
+	
+	var filelist="";
+	if (!userLoginUUID[url_val.loginUUID]) {
+		filelist="No Login";
+	}else {
+		filelist = showDir();
+	}
+	
+	ejs.renderFile( ___dir + "/file.html", { dirlist: filelist }, function(err, str) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.write(str);
+			res.end();
+		}
+    });
+	return;
+}
+
+function showDir(){
+	file="";
+	dir="";
+
+	//读取目录
+	const items=fs.readdirSync(___path);
+	
+	items.forEach(item => {
+		const stat = fs.statSync(___path +"/"+ item);
+		var item_date = stat.mtime.toString().split(" "); 
+		var item_size = stat.size.toString(); 
+		//item_date_arr = item_date.toString().split(/\s+/); 
+		//是否为文件夹
+		if (stat.isDirectory()) {
+			dir = dir + '<br/>' + item;	
+		} else {
+			file = file + '<br/>' + item;
+		}
+	});
+	
+	return dir + '<br/>' + file;
+}
+
+function errHtml(res,code,msg){
+	res.writeHead(code, {'Content-Type': 'text/html; charset=utf-8'});
+	res.end(msg);
+}
+
+function loadfile(res,req){
+	var headType = ___path.substr(___path.lastIndexOf(".") + 1, ___path.length);
+	
+	fs.readFile(___path, function (err, data) {
+		if (err) {
+			//404
+			errHtml(res,404,"err");
+			return;
+		}else{
+			if (headType=="gif" || headType=="png" || headType=="jpg") {
+				res.writeHead(200, {'Content-Type': 'image/' + headType});
+			} else if (headType=="svg") {
+				res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+			} else if (headType=="js") {
+				res.writeHead(200, {'Content-Type': 'application/javascript'});
+			} else if (headType=="mp4") {
+				res.writeHead(200, {'Content-Type': 'video/' + headType});
+			} else {
+				res.writeHead(200, {'Content-Type': 'text/' + headType});
+			}
+			res.write(data);
+		}
+		res.end();
+	});
+	return;
+}
+
+
+console.log('OK! Server running at http://127.0.0.1:' + port + '/');
+console.log('____________________________________________\n');
---
+++ run-fileact.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+powershell node fileact.js
+pause
